5280de7b00ed3a36db1f348483f0effc6bcacd49
Lucas Guzzo <ext.lucas.guzzo@farfetch.com>
2019-06-29 02:38:23 +0100

    Add identifier attribute

---
+++ src/InMemoryDatabase/Identifier/IdentifierAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace InMemoryDatabase.Identifier
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    public class IdentifierAttribute : Attribute
+    {
+        public int Order { get; set; }
+
+        public IdentifierAttribute(int order = 1)
+        {
+            Order = order;
+        }
+    }
+}
