e76f8578a738a48d5870b651ffcaf2d7aa686fb4
Lucas Guzzo <ext.lucas.guzzo@farfetch.com>
2019-06-30 00:31:48 +0100

    Add simple API project with inmemory database

---
+++ samples/InMemoryDatabase.Sample/Controllers/StudentController.cs
@@ -0,0 +1,64 @@
+﻿namespace InMemoryDatabase.Sample.Controllers
+{
+    using System.Collections.Generic;
+    using InMemoryDatabase.Sample.Models;
+    using InMemoryDatabase.Sample.Repositories;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        // GET: api/Student
+        [HttpGet]
+        public ActionResult<IEnumerable<StudentDTO>> Get(string name = null, int? classNumber = null, int? studentNumber = null)
+        {
+            return Ok(_studentRepository.Get(name, classNumber, studentNumber));
+        }
+
+        // GET: api/Student/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<StudentDTO> Get(string id)
+        {
+            try
+            {
+                var student = _studentRepository.Get(id);
+                return Ok(student);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST: api/Student
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] StudentDTO value)
+        {
+            return Ok(_studentRepository.Save(value));
+        }
+
+        // PUT: api/Student/5
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] StudentDTO value)
+        {
+            _studentRepository.Update(value);
+            return NoContent();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            _studentRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ samples/InMemoryDatabase.Sample/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace InMemoryDatabase.Sample.Models
+{
+    using InMemoryDatabase.Attributes;
+
+    public class Student
+    {
+        [Identifier]
+        public int ClassNumber { get; set; }
+
+        [Identifier(2)]
+        public int StudentNumber { get; set; }
+
+        public string Name { get; set; }
+    }
+}
