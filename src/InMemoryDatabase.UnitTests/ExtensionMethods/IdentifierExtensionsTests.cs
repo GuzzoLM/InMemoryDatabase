2981476d57ec6fe45e2856f75dfe2cc9103827b0
Lucas Guzzo <ext.lucas.guzzo@farfetch.com>
2019-06-29 13:05:49 +0100

    Add tests to extension methods

---
+++ src/InMemoryDatabase.UnitTests/ExtensionMethods/IdentifierExtensionsTests.cs
@@ -0,0 +1,119 @@
+﻿namespace InMemoryDatabase.UnitTests.ExtensionMethods
+{
+    using System;
+    using FluentAssertions;
+    using InMemoryDatabase.Attributes;
+    using InMemoryDatabase.Exceptions;
+    using InMemoryDatabase.Extensions;
+    using Xunit;
+
+    public class IdentifierExtensionsTests
+    {
+        [Fact]
+        public void GetIdentifier_ClassWithOneIdProperty_ShouldPass()
+        {
+            // Arrange
+            var entity = new ClassWithOneIdentifier
+            {
+                Id = Guid.NewGuid(),
+                Name = "Lucas"
+            };
+
+            // Act
+            Action act = () => entity.GetIdentifier();
+
+            // Assert
+            act.Should()
+                .NotThrow();
+        }
+
+        [Fact]
+        public void GetIdentifier_ClassWithTwoIdProperty_ShouldPass()
+        {
+            // Arrange
+            var entity = new ClassWithTwoIdentifiers
+            {
+                FirstName = "Lucas",
+                LastName = "Poley"
+            };
+
+            // Act
+            Action act = () => entity.GetIdentifier();
+
+            // Assert
+            act.Should()
+                .NotThrow();
+        }
+
+        [Fact]
+        public void GetIdentifier_ClassWithTwoIdPropertyWithSameOrder_ShouldThrow()
+        {
+            // Arrange
+            var entity = new ClassWithTwoIdentifiersInSameOrder
+            {
+                FirstName = "Lucas",
+                LastName = "Poley"
+            };
+
+            // Act
+            Action act = () => entity.GetIdentifier();
+
+            // Assert
+            act.Should()
+                .Throw<InvalidIdentityException>()
+                .WithMessage("Found identifier properties with same order number");
+        }
+
+        [Fact]
+        public void GetIdentifier_ClassWithNoIdProperty_ShouldThrow()
+        {
+            // Arrange
+            var entity = new ClassWithNoIdentifiers
+            {
+                FirstName = "Lucas",
+                LastName = "Poley"
+            };
+
+            // Act
+            Action act = () => entity.GetIdentifier();
+
+            // Assert
+            act.Should()
+                .Throw<InvalidIdentityException>()
+                .WithMessage("Class must have an Identifier");
+        }
+
+        class ClassWithOneIdentifier
+        {
+            [Identifier]
+            public Guid Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        class ClassWithTwoIdentifiers
+        {
+            [Identifier]
+            public string FirstName { get; set; }
+
+            [Identifier(2)]
+            public string LastName { get; set; }
+        }
+
+        class ClassWithTwoIdentifiersInSameOrder
+        {
+            [Identifier]
+            public string FirstName { get; set; }
+
+            [Identifier]
+            public string LastName { get; set; }
+        }
+
+        class ClassWithNoIdentifiers
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+        }
+    }
+}
