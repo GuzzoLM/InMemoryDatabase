32798fbd79fa75512ebd447a8bb1b0a704fe3080
Lucas Guzzo <ext.lucas.guzzo@farfetch.com>
2019-06-30 22:40:15 +0100

    Document Setup

--- src/InMemoryDatabase/Setup/SetupCollections.cs
+++ src/InMemoryDatabase/Setup/SetupCollections.cs
@@ -5,6 +5,10 @@
 
     public static class SetupCollections
     {
+        /// <summary>
+        /// Register an in-memory collection on services collection.
+        /// </summary>
+        /// <typeparam name="T">The type of class that will be persisted in the collection.</typeparam>
         public static IServiceCollection SetupInMemoryCollection<T>(this IServiceCollection services)
         {
             return services.AddSingleton<IInMemoryCollection<T>, InMemoryCollection<T>>();
